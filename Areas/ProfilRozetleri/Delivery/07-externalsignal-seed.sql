53196dfce6666742500995e0e2a01671bd3f87ff
Zeynep Akbulut <zeynepakbulut3400@gmail.com>
2026-08-27 12:14:06 +0300

    remove delivery folder

--- Areas/ProfilRozetleri/Delivery/07-externalsignal-seed.sql
+++
@@ -1,8 +0,0 @@
-﻿/* =====================================================================
-   Ornek ExternalSignal rozeti: Wizard turunu tamamlama. Idempotent.
-   ===================================================================== */
-
-INSERT INTO dbo.Badges (BadgeName, BadgeDescription, IconPath, BadgeType, RequiredValue, ModuleId, ExternalSignalKey)
-SELECT N'Wizard Ustası', N'Tanıtım turunu baştan sona tamamladın.', N'/Content/Badges/25-gold-jade-r5.png', 3, 1, NULL, N'wizard-tour-completed'
-WHERE NOT EXISTS (SELECT 1 FROM dbo.Badges WHERE BadgeName = N'Wizard Ustası');
-GO
